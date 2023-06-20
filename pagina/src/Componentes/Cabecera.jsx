3fe216b314c9dc7a92a7f9f68f92854036210274
unknown <jbarrioso3@miumg.edu.gt>
2023-06-20 12:44:43 -0600

    Menu de Navegacion

--- pagina/src/Componentes/Cabecera.jsx
+++ pagina/src/Componentes/Cabecera.jsx
@@ -1,17 +1,22 @@
-import './Cabecera.css';
+import { useState } from 'react';
+import  './Cabecera.css';
 
 const Cabecera = () => {
+    const [menu, setMenu] = useState(false)
+    const toggleMenu = () =>{
+        setMenu(!menu)
+    }
     return(
         <header className="Cabecera"> {/*header.Cabecera*/}
             <h1 className="Cabecera-h1"> {/*h1.Cabecera-h1*/}
-                <a href="" className="Cabera-a">Probando</a> {/*a.Cabecera-a*/}
+                <a href="" className="Cabera-a">Certificados</a> {/*a.Cabecera-a*/}
             </h1>
-            <button className="Cabecera-button"> {/*button.Cabecera-button*/}
+            <button  onClick={toggleMenu} className="Cabecera-button"> {/*button.Cabecera-button*/}
                 <svg className='Cabecera-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                 </svg>
             </button>
-            <nav className="Cabera-nav"> {/*nav.Cabecera-nav*/}
+            <nav className= {`Cabecera-nav ${ menu ? 'isActive' : ''}`}> {/*nav.Cabecera-nav*/}
                 <ul className="Cabecera-ul">
                    <li className="Cabecera-li"><a href="" className="Cabecera-a">Item 1</a></li> {/*li.Cabecera-li*5>a.Cabecera-a{Item $}*/}
                    <li className="Cabecera-li"><a href="" className="Cabecera-a">Item 2</a></li>
