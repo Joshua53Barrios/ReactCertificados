20ff98d86b0fb7cbe3849b397c0033f35990fa5a
Yordi <superyo234@gmail.com>
2023-07-04 10:38:26 -0600

    se me olvido los botonesXD

--- pagina/src/Componentes/walllets.js
+++ pagina/src/Componentes/walllets.js
@@ -235,7 +235,7 @@ render(){
     <br />
       <div id='breadcrumb'>
       <h1>Wallets</h1>
-      <button className='btn btn-success' style={{width:'150px', height:'50px' }} onClick={()=>{this.setState({form: null, tipoModal: 'insert'}); this.modalInsertar()}}><BiSolidPlusCircle/> Add Domain</button>
+      <button className='btn btn-success' style={{width:'160px', height:'50px' }} onClick={()=>{this.setState({form: null, tipoModal: 'insert'}); this.modalInsertar()}}><BiSolidPlusCircle/> Add Domain</button>
       </div>
       <br /> <br />
       <table className='table'>
